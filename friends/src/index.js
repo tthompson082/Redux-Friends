2d6aad8aae9f9e4e6b7dbf16e4da3a172b393290
Tyler Thompson <tylerthompson@Tylers-MacBook-Pro.local>
2019-06-19 16:48:09 -0400

    added functionality for redux dev tools

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
     rootReducer,
-    applyMiddleware(thunk)
+    composeEnhancers(applyMiddleware(thunk))
 )
 
 ReactDOM.render(
