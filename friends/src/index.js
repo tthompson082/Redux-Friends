50b5b1a49647442c97c06f5123ffd0035c495cf3
Tyler Thompson <tylerthompson@Tylers-MacBook-Pro.local>
2019-06-19 16:13:48 -0400

    aded required dependencies and reactstrap for styling, set up index.js with store and provider, as well as applyMiddleware

---
+++ friends/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from './reducers';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk)
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
+
