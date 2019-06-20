f018ec16e40efa930a2b0d94c4cbe93d63e8a945
Tyler Thompson <tylerthompson@Tylers-MacBook-Pro.local>
2019-06-20 15:44:58 -0400

    added navbar to top of screen

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import { LoginView } from './views';
+import FriendsNavbar from './components/Navbar';
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <FriendsNavbar />
         <LoginView />
       </div>
     )
---
+++ friends/src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+class FriendsNavbar extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            collapsed: true
+        }
+    }
+    toggleNavbar = () => {
+        this.setState({
+            collapsed: !this.state.collapsed
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar color='secondary' light>
+                    <NavbarBrand className='mr-auto text-light'><h1>Friends List!</h1></NavbarBrand>
+                    <NavbarToggler onClick={this.toggleNavbar} className='mr-2 bg-light' />
+                    <Collapse isOpen={!this.state.collapsed} navbar>
+                        <Nav navbar>
+                            <NavItem>
+                                <NavLink href='/friends' className='text-light'><h3>Friends</h3></NavLink>
+                            </NavItem>
+                            <NavItem>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        )
+    }
+}
+
+export default FriendsNavbar;
